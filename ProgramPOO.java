34a5b8ec7d315de603932d3e1bd3c02308623596
AndreLjesus <143151297+AndreLjesus@users.noreply.github.com>
2025-06-06 10:57:14 -0300

    atualicação da classe Triangle

--- ProgramPOO.java
+++ ProgramPOO.java
@@ -3,7 +3,7 @@ import entities.Triangle;
 
 public class ProgramPOO {
     public static void main(String[] args) {
-        double p, area1, area2;
+        double area1, area2;
         Scanner sc = new Scanner(System.in);
         Triangle x, y;
         x = new Triangle();
@@ -13,19 +13,15 @@ public class ProgramPOO {
         x.a = sc.nextDouble();
         x.b = sc.nextDouble();
         x.c = sc.nextDouble();
-
-        p = (x.a + x.b + x.c) / 2;
-        area1 = Math.sqrt(p * (p - x.a) * (p - x.b) * (p - x.c));
+        area1 = x.area();
 
         System.out.println("Digite os lados do triangulo Y: ");
         y.a = sc.nextDouble();
         y.b = sc.nextDouble();
         y.c = sc.nextDouble();
+        area2 = y.area();
 
         sc.close();
-
-        p = (y.a + y.b + y.c) / 2;
-        area2 = Math.sqrt(p * (p - y.a) * (p - y.b) * (p - y.c));
 
         if (area1 > area2){
             System.out.println("O triangulo A é maior");
--- entities/Triangle.java
+++ entities/Triangle.java
@@ -1,7 +1,18 @@
-package entities;
+package entities; // Pacote
 
-public class Triangle{
+public class Triangle{ // Nome da classe
+    // Publuc indica que pode ser acessado por outros arquivos
+    // a, b, c -> Atributos da classe Triangle
     public double a;
     public double b;
     public double c;
+
+    // Criar função
+    // Double significa que essa função retorna um valor double
+    // area é o nome do metodo e a lista de paramentros
+    public double area(){
+        double p = (a + b + c) / 2;
+        double result = Math.sqrt(p * (p - a) * (p - b) * (p - c));
+        return result;
+    }
 }
